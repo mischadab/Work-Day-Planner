4bf5cdddbd33639a5312c2303069ca8404ec566b
mischadab <mischadabrowski@gmail.com>
2021-01-30 13:25:46 -0500

    created save to local storage

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ $(document).ready(function () {
     // display current day
     $("#currentDay").text(moment().format('LL'));
 
+    // getting the button to save
+    $(".saveBtn").on("click", function(){
+        // grab values that are entered into the textarea
+        var entry = $(this).siblings(".description").val();
+        var timeBl = $(this).parent().attr("id");
+
+        // put in local storage
+        localStorage.setItem(timeBl, entry);
+    })
+
 
 
 
