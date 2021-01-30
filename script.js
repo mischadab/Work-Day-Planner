2e6de7e5f499938b345d8df0777e48c0fb6e84ec
mischadab <mischadabrowski@gmail.com>
2021-01-30 13:55:25 -0500

    colour coding function added

--- script.js
+++ script.js
@@ -10,16 +10,32 @@ $(document).ready(function () {
 
         // put in local storage
         localStorage.setItem(timeBl, entry);
-    })
+    });
 
+        function colorCode(){
+            // get current hour that day
+            var presentHour = moment().hours();
 
+            // loop through the time blocks using .each() method
+            $(".time-block").each(function(){
+                var timeSlot = parseInt($(this).attr("id").split("-")[1]);
 
-
-
-
-
-
-
-
+                 // for past time coloration
+                if(timeSlot < presentHour){
+                    $(this).addClass("past")
+                } else if (timeSlot === presentHour) {
+                    $(this).removeClass("past");
+                    $(this).addClass("present");
+                } else {
+                    $(this).removeClass("past");
+                    $(this).removeClass("present");
+                    $(this).addClass("future");
+                }
+            });
+        }
+        colorCode();
+    
+    // get items from localStorage
+    
     
 });
